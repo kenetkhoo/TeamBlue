6cbff2e8710b873a3f774adddc668aa3d16ab714
kyngoctran <kyngoctran@gmail.com>
2014-04-16 19:20:47 -0700

    added android features and fixed some stuff
    +accelerometer
    +boundaries

--- New Unity Project/Assets/Scripts/PlayerScript.cs
+++ New Unity Project/Assets/Scripts/PlayerScript.cs
@@ -27,8 +27,17 @@ public class PlayerScript : MonoBehaviour {
 
 	void FixedUpdate()
 	{
+		//boundaries control
+		Vector3 playerSize = GetComponent<SpriteRenderer> ().sprite.bounds.size;
+		float viewWidth = (Screen.width * Camera.main.orthographicSize) / Screen.height;
+		float relWidth = (playerSize.x / 2) / viewWidth / 2;
+		Vector3 viewPos = Camera.main.WorldToViewportPoint (this.transform.position);
+		viewPos.x = Mathf.Clamp (viewPos.x, 0+relWidth, 1-relWidth);
+		this.transform.position = Camera.main.ViewportToWorldPoint (viewPos);
 		// 5 - Move the game object
 		rigidbody2D.velocity = movement;
+		//accelerometer control
+		rigidbody2D.transform.Translate(Input.acceleration.x*0.4f,0,0);
 	}
 
 	void OnTriggerEnter2D(Collider2D otherCollider)
