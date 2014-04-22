2466e3454a99bdb60d605fc69dfb293a9df29b53
kyngoctran <kyngoctran@gmail.com>
2014-04-21 21:52:58 -0700

    TitleScreen
    I added a simple title screen. It is still a bit buggy

---
+++ New Unity Project/Assets/Scripts/ExpandText.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExpandText : MonoBehaviour {
+	public float scale;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(scale <= 1.0f)
+			scale = scale + 0.05f;
+		if (scale < 1.0f)
+			transform.localScale += new Vector3(0.05f,0.05f,0);
+	}
+}
---
+++ New Unity Project/Assets/Scripts/TitleScreenScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScreenScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void OnGUI()
+	{
+
+		if(GUI.Button (new Rect(Screen.width / 2 - 30, Screen.height/4, 60, 30), "Start"))
+		{
+			Application.LoadLevel("Stage1");
+		}
+	}
+}
