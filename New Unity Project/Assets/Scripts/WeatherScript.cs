c45801f612f274a384c154ba829a4524c4b9af6b
elaine lo <elaine@thelos.us>
2014-04-29 23:36:45 -0700

    weather script

---
+++ New Unity Project/Assets/Scripts/WeatherScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeatherScript : MonoBehaviour {
+	
+	
+	
+	WWW weatherWebsite;
+	bool Done = true;
+	string urlSave;
+	
+	// Use this for initialization
+	void Start () {
+		string loc = "Pomona";
+		string url = "http://api.worldweatheronline.com/free/v1/weather.ashx?q=" + loc + "&format=xml&num_of_days=1&key=u247zy5k7n3bktwyfyqgesfq";
+		weatherWebsite = new WWW (url);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(weatherWebsite.isDone && Done)
+		{
+			urlSave = weatherWebsite.text;
+			print (urlSave);
+			Done = false;
+		}
+		
+	}
+}
