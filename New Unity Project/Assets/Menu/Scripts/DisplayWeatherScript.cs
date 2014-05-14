f812c14a9e60e21506b4211fbc299b5c95db3892
kenetkhoo <kenetkhoo@gmail.com>
2014-05-14 02:09:51 -0700

    GPS fully functional
    combined weather and gps scripts.  updated call.  fixed errors.

--- New Unity Project/Assets/Menu/Scripts/DisplayWeatherScript.cs
+++ New Unity Project/Assets/Menu/Scripts/DisplayWeatherScript.cs
@@ -6,7 +6,12 @@ public class DisplayWeatherScript : MonoBehaviour
 {
 	float width;
 	float height;
-	
+
+	public float lon;
+	public float lat;
+	public string retrievedCoordinates;
+	public string retrievedCoordinates2;
+	public string retrievedCitys;
 	public string currentIP;
 	public string currentCity;
 	public float Fahrenheit;
@@ -17,8 +22,32 @@ public class DisplayWeatherScript : MonoBehaviour
 	public string conditionName;
 		
 	// Use this for initialization
-	void Start ()
+	IEnumerator Start ()
 	{
+		if (!Input.location.isEnabledByUser)
+			yield break;
+		Input.location.Start(1,1);
+		
+		int maxWait = 20;
+		while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0) {
+			yield return new WaitForSeconds(1);
+			maxWait--;
+		}
+		if (maxWait < 1) {
+			print("Timed out");
+			yield break;
+		}
+		if (Input.location.status == LocationServiceStatus.Failed) {
+			print("Unable to determine device location");
+			yield break;
+		} else{
+			
+			lat = Input.location.lastData.latitude;
+			lon = Input.location.lastData.longitude; 
+			print("Location: " + lat + " " + lon + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
+			//print("Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
+		}
+		Input.location.Stop();
 		width = ((Screen.width / 2) * Camera.main.pixelWidth) / Screen.width;
 		height =  ((Screen.height/ 10) * Camera.main.pixelHeight) / Screen.height;
 		StartCoroutine(SendRequest());
@@ -46,17 +75,19 @@ public class DisplayWeatherScript : MonoBehaviour
         }
 	*/
 		
-		GPS g = GetComponent<GPS>();
-		
+
 		//get the current weather; search by lat and lon api.openweathermap.org/data/2.5/weather?lat=??&lon=??
-		WWW request = new WWW("api.openweathermap.org/data/2.5/weather?lat=" + g.lat + "&lon=" + g.lon); 
+		WWW request = new WWW("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon); 
         //WWW request = new WWW("http://api.openweathermap.org/data/2.5/weather?q=" + currentCity); 
         yield return request;
 		
 		if (request.error == null || request.error == "")
 		{
 			var N = JSON.Parse(request.text);
-			
+			retrievedCoordinates = N["coord"]["lon"].Value; //longitude
+			retrievedCoordinates2 = N["coord"]["lat"].Value; //longitude
+			retrievedCitys = N["name"].Value; //get the city
+			print (retrievedCitys);
 			temp = N["main"]["temp"].Value; //get the temperature
             float tempTemp; //variable to hold the parsed temperature
             float.TryParse(temp, out tempTemp); //parse the temperature
@@ -71,7 +102,7 @@ public class DisplayWeatherScript : MonoBehaviour
 	
 	void OnGUI()
 	{
-		GUI.Label(new Rect(Screen.width / 2.0f , Screen.height/5.5f, width, height), Celsius.ToString() + "C");
-		GUI.Label(new Rect(Screen.width / 3.5f , Screen.height/5.5f, width, height), Fahrenheit.ToString() + "F");
+	//	GUI.Label(new Rect(Screen.width / 2.0f , Screen.height/5.5f, width, height), Celsius.ToString() + "C");
+		GUI.Label(new Rect(Screen.width / 6.5f , Screen.height/5.5f, width, height), Fahrenheit.ToString() + "F" + Celsius.ToString() + "C" + " " +retrievedCitys + " " + lon +"lon" + lat+ "lat");
     }
 }
