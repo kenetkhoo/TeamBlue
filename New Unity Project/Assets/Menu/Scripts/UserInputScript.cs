721eaf5c6ab356cb991882a920fb2bcff005331a
kyngoctran <kyngoctran@gmail.com>
2014-05-20 19:54:12 -0700

    userinput
    I fixed userinput after running it on my phone. GuiTextField will bring
    up the android keyboard automatically. TouchScreenKeyboard is only used
    when you want to force the keyboard to come up.

--- New Unity Project/Assets/Menu/Scripts/UserInputScript.cs
+++ New Unity Project/Assets/Menu/Scripts/UserInputScript.cs
@@ -6,27 +6,21 @@ public class UserInputScript : MonoBehaviour {
 	float width;
 	float height;
 
-	public string cityName = "";
-	public TouchScreenKeyboard keyboard;
+	public string cityName = "Enter a city";
 	
 	void Start () {
-		keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default, true, false, false, false, "" );
-		keyboard.active = false;
+
+
 	}
 	
 	void Update () {
 
 	}
 	
-	void OnGUI() {	
-		cityName = GUI.TextField(new Rect(Screen.width / 5.0f , Screen.height/2.0f, 200, 30), " Enter city name");
-		keyboard.active = true;
-		keyboard = TouchScreenKeyboard.Open(cityName, TouchScreenKeyboardType.Default, true, false, false, false, "");
-		
-		if(keyboard.done){
-			cityName = keyboard.text;
-			keyboard.active = false;
-		}
+	void OnGUI() {
+		cityName  = GUI.TextField (new Rect (Screen.width / 5.0f, Screen.height / 2.0f, 200, 30), cityName);
+			
+
 	}
 	
 	
